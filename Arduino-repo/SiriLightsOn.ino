68c8df311d247984fc7e5f536ada6e273078002b
KarthikMB <36357510+Ikarthikmb@users.noreply.github.com>
2020-07-26 18:18:24 +0530

    Update SiriLightsOn.ino

--- Arduino-repo/SiriLightsOn.ino
+++ Arduino-repo/SiriLightsOn.ino
@@ -3,8 +3,8 @@
 #include <ESP8266WebServer.h>
 #include <ESP8266mDNS.h>
 
-const char* ssid = "Ocean";
-const char* password = "GreyStorm";
+const char* ssid = "ssid"; //enter ssidd here
+const char* password = "Password"; //enter passwor here
 const int relayPin = D4;
 
 ESP8266WebServer server(80);
